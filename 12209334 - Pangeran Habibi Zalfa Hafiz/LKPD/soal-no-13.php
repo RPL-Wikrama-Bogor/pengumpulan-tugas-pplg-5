912b118de177e9be9a6b20f58da7cd708eee5213
pangeranhabibi <pangeranhabibi162@gmail.com>
2023-08-30 11:03:41 +0700

    Pangeran

---
+++ 12209334 - Pangeran Habibi Zalfa Hafiz/LKPD/soal-no-13.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php
+  for ($i = 1; $i <= 50; $i++) {
+    echo " $i, ";
+  }
+  ?>
+   <br>
+        <a href="soal-no-12.php">Soal Sebelumnya</a>
+        <br>
+        <a href="soal-no-14.php">Soal Berikutnya</a>
+</body>
+
+</html>
