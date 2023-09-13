d731996db016ef0231f056736527ca947fcc960a
rizkym4l <rizkymaulana.more@gmail.com>
2023-09-13 08:36:04 +0700

    nambah tugas

--- 12209394 - Rizki Maulana Arif/array/arraytugass.php
+++ 12209394 - Rizki Maulana Arif/array/arraytugass.php
@@ -2,42 +2,65 @@
 $tabungan = [10000, 50000, 10000, 5000, 20000, 5000, 50000, 20000];
 
 // no 1
-echo "Uang yang terdapat di tabungan saya adalah <b> " . implode(", ", $tabungan) . "<br>". "<br>";
-
+echo "Uang yang terdapat di tabungan saya adalah <b> " . implode(", ", $tabungan) . "</b>";
+echo " <br> [][][][][][][][][][ <br>";
 
 // no 2
-echo "Jumlah tabungan saya adalah <b> " . array_sum($tabungan) . "<br>". "<br>";
-
+echo "Jumlah tabungan saya adalah <b> " . array_sum($tabungan) . "</b>";
+echo " <br> [][][][][][][][][][ <br>";
 
 // no 3
 $pecahan_unik = array_unique($tabungan); // menghapus duplikat
-echo "Pecahan yang terdapat di tabungan saya adalah <b> " . $pecahan_unik_implode = implode(", ", $pecahan_unik) . "<br>". "<br>";
+echo "Pecahan yang terdapat di tabungan saya adalah <b> " . $pecahan_unik_implode = implode(", ", $pecahan_unik) . "</b>";
+echo " <br> [][][][][][][][][][ <br>";
 
+// no 4
+echo "Pecahan uang terkecil adalah <b> " . min($tabungan) . "</b>";
+echo " <br> [][][][][][][][][][ <br>";
 
+// no 5
+echo "Pecahan uang terbesar adalah <b> " . max($tabungan) . "</b>";
+echo " <br> [][][][][][][][][][ <br>";
 
-echo "Pecahan uang terkecil adalah <b> " . min($tabungan) . "<br>". "<br>";
+// no 6
+$tabungan_baru = $tabungan;
+asort($tabungan_baru);
+echo "Jika diurutkan dari yang terkecil maka uang yang berada di tabungan adalah <b> " . implode(", ", $tabungan_baru) . "</b>";
+echo " <br> [][][][][][][][][][ <br>";
 
-echo "Pecahan uang terbesar adalah <b> " . max($tabungan) . "<br>". "<br>";
+// no 7
+arsort($tabungan_baru);
+echo "Jika diurutkan dari yang terbesar maka uang yang berada di tabungan adalah <b> " . implode(", ", $tabungan_baru) . "</b>";
+echo " <br> [][][][][][][][][][ <br>";
 
-$tabunganberurutan =  $tabungan;
-asort($tabunganberurutan);
-echo "Jika diurutkan dari yang terkecil maka uang yang berada di tabungan adalah <b> " . implode(", ", $tabunganberurutan) . "<br>". "<br>" ;
+// no 8
+echo "saya ingin mengganti pecahan <b> 50000 </b> yang ada di tabungan menjadi <b> 100000 </b> <br>";
+$tabungan_baru = [];
+$gabung = false;
 
-arsort($tabungan);
-echo "Jika diurutkan dari yang terbesar maka uang yang berada di tabungan adalah <b> " . implode(", ", $tabunganberurutan) . "<br>". "<br>";
-echo "saya ingin mengganti pecahan <b> 50000 <br> yang ada di tabungan menjadi <br> 100000 <br>";
+foreach ($tabungan as $nilai) {
+  if ($nilai == 50000 && !$gabung) {
+    $tabungan_baru[] = 100000;
+    $gabung = true;
+  } elseif ($nilai == 50000 && $gabung) {
+  } else {
+    $tabungan_baru[] = $nilai;
+  }
+}
 
-array_splice($tabungan, 0, 2, 100000);
+echo "maka kini uang yang terdapat di tabungan saya adalah <b> " . implode(', ', $tabungan_baru) . "</b>";
+echo " <br> [][][][][][][][][][ <br>";
 
-shuffle($tabungan);
-echo "maka kini uang yang terdapat di tabungan saya adalah <b> " . implode(', ', $tabungan ) . "<br>". "<br>";
+// no 9 
+array_push($tabungan_baru, 20000);
+echo "Uang yang terdapat di tabungan saya adalah <b> " . implode(", ", $tabungan_baru) . "</b>";
+echo " <br> [][][][[][][][] <br>";
 
+// no 10 
+asort($tabungan_baru);
+echo "Jika diurutkan dari yang terkecil kini tabungan saya adalah <b> " . implode(", ", $tabungan_baru) . "</b>";
+echo " <br> [][][][[][][][] <br>";
 
-array_push($tabungan, 20000);
-echo "Uang yang terdapat di tabungan saya adalah <b> " . implode(", ", $tabungan) . "<br>". "<br>";
-
-asort($tabungan);
-echo "Jika diurutkan dari yang terkecil kini tabungan saya adalah <b> " . implode(", ", $tabungan) . "<br>". "<br>";
-
-echo "Maka jumlah tabungan saya saat ini adalah <b> " . array_sum($tabungan) . "<br>". "<br>";
-
+// no 11
+echo "Maka jumlah tabungan saya saat ini adalah <b> " . array_sum($tabungan_baru) . "</b>";
+echo " <br> [][][][[][][][]<br>";
