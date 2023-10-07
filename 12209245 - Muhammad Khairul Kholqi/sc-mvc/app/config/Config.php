de94917af206389f2b06613827ade193a7815c64
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2023-10-07 20:18:52 +0700

    Uplode tugas MVC

---
+++ 12209245 - Muhammad Khairul Kholqi/sc-mvc/app/config/Config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('BASE_URL', 'http://localhost/kelas-11/kelas-11-mvc/sc-mvc/public');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'db_sc_pinjaman');
---
+++ 12209245 - Muhammad Khairul Kholqi/sc-mvc/app/models/BukuModel.php
@@ -0,0 +1,74 @@
+    <?php
+
+    class BukuModel {
+        private $table = 'tb_pinjaman';
+        private $db;
+
+        public function __construct()
+        {
+            $this->db = new Database;
+        }
+
+        public function getAllBarang()
+        {
+            $this->db->query("SELECT * FROM " . $this->table);
+            return $this->db->resultSet();
+        }
+
+        public function getBarangById($id)
+        {
+            $this->db->query('SELECT * FROM ' . $this->table . ' WHERE id=:id');
+            $this->db->bind('id', $id);
+            return $this->db->single();
+        }
+
+        public function tambahElektronik($data)
+        {
+            $query = "INSERT INTO tb_pinjaman (nama_peminjam, jenis_barang, no_barang, tgl_pinjam) VALUE(:nama_peminjam, :jenis_barang, :no_barang, :tgl_pinjam)";
+            $this->db->query($query);
+            $this->db->bind('nama_peminjam', $data['nama_peminjam']);
+            $this->db->bind('jenis_barang', $data['jenis_barang']);
+            $this->db->bind('no_barang', $data['no_barang']); 
+            $this->db->bind('tgl_pinjam', $data['tgl_pinjam']); 
+            $this->db->execute();
+
+            return $this->db->rowCount();
+        }
+
+        public function updateElektronik($data)
+        {
+            $query = "UPDATE tb_pinjaman SET nama_peminjam=:nama_peminjam, jenis_barang=:jenis_barang, no_barang=:no_barang, tgl_pinjam=:tgl_pinjam, tgl_kembali=:tgl_kembali WHERE id=:id";
+            $this->db->query($query);
+            $this->db->bind('id', $data['id']);
+            $this->db->bind('nama_peminjam', $data['nama_peminjam']);
+            $this->db->bind('jenis_barang', $data['jenis_barang']);
+            $this->db->bind('no_barang', $data['no_barang']); 
+            $this->db->bind('tgl_pinjam', $data['tgl_pinjam']); 
+            $this->db->bind('tgl_kembali', $data['tgl_kembali']); 
+            $this->db->execute();
+        
+            return $this->db->rowCount();
+        }
+        
+
+        public function deleteBarang($id)
+        {
+            $this->db->query('DELETE FROM ' . $this->table . ' WHERE id=:id');
+            $this->db->bind('id', $id);
+            $this->db->execute();
+
+            return $this->db->rowCount();
+        }
+
+        public function cariBuku($elektronik) {
+            $this->db->query("SELECT * FROM " . $this->table . " WHERE nama_peminjam LIKE :namaPeminjam OR jenis_barang LIKE :jenisBarang OR no_barang LIKE :noBarang OR tgl_pinjam LIKE :tglPinjam OR tgl_kembali LIKE :tglKembali");
+            $this->db->bind('namaPeminjam', '%' . $elektronik . '%');
+            $this->db->bind('jenisBarang', '%' . $elektronik . '%');
+            $this->db->bind('noBarang', '%' . $elektronik . '%');
+            $this->db->bind('tglPinjam', '%' . $elektronik . '%');
+            $this->db->bind('tglKembali', '%' . $elektronik . '%');
+            return $this->db->resultSet();
+        }
+    }
+
+    
