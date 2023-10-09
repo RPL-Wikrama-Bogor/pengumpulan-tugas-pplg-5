9e5717fe2726e0167efc416c67dada22c49c4196
projectSimon <simonsaputra2908@gmail.com>
2023-10-09 12:33:42 +0700

    phpmvcpeminjaman

---
+++ 12209416 - Simon/phpmvcpeminjaman/app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+define('BASE_URL', 'http://localhost/kelas%2011/phpmvcpeminjaman/public');
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','db_mvc_peminjaman');
