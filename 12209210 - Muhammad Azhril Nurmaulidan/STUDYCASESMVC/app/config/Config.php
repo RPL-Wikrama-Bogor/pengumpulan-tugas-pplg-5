529bff83dc2ded5125701f744108a60dd5a75076
lirhzaaa <mhmdazhrilnmldn@gmail.com>
2023-10-09 12:22:45 +0700

    tugas mvc

---
+++ 12209210 - Muhammad Azhril Nurmaulidan/STUDYCASESMVC/app/config/Config.php
@@ -0,0 +1,8 @@
+<?php 
+
+define('BASE_URL', 'http://localhost/STUDYCASESMVC/public');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'tugas_mvc');
