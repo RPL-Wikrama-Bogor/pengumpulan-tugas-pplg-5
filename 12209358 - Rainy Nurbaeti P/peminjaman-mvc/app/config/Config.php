66597ee8cbe8c067fd6bb8c0b2d60ceae74cd85c
rainynurbaeti <rainynurbaetiputri@smkwikrama.sch.id>
2023-10-13 14:20:42 +0700

    mvc new

---
+++ 12209358 - Rainy Nurbaeti P/peminjaman-mvc/app/config/Config.php
@@ -0,0 +1,8 @@
+<?php
+// konstanta = nilai nya tidak bisa berubah
+define('BASE_URL' , 'http://localhost/phpmvc_peminjaman/public') ;
+
+define('DB_HOST' , 'localhost') ;
+define('DB_USER' , 'root') ;
+define('DB_PASS' , '') ;
+define('DB_NAME' , 'db_oop_mvc_1') ;
