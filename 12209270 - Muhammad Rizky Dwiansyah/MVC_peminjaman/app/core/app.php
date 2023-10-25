794ca300283ad2e97cb4845a23e57dc6b5c41bda
ikkyyyyy <muhammadrizkydwiansyah@smkwikrama.sch.id>
2023-10-25 16:04:59 +0700

    MVC

---
+++ 12209270 - Muhammad Rizky Dwiansyah/MVC_peminjaman/app/core/app.php
@@ -0,0 +1,40 @@
+<?php
+
+class App {
+    protected $controller = 'home';
+    protected $method = 'index';
+    protected $params = [];
+
+    public function __construct() {
+        $url = $this->parseUrl();
+
+        if (file_exists('../app/controllers/' . $url[0] . '.php')) {
+            $this->controller = $url[0];
+            unset($url[0]);
+        }
+
+        require_once '../app/controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        if (isset($url[1])) {
+            if (method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        $this->params = $url ? array_values($url) : [];
+
+        call_user_func_array([$this->controller, $this->method], $this->params);
+    }
+
+    protected function parseUrl() {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url);
+            $url = explode('/', $url);
+            return $url;
+        }
+        return [];
+    }
+}
