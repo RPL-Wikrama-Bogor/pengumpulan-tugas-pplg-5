5274222010fef4cd8d287447760b4135b2ef28ab
oktaharis <oktaharissutanto@smkwikrama.sch.id>
2023-11-01 14:01:20 +0700

    vue-CARD

---
+++ 12209331 - Okta Haris Sutanto/vue-CARD/src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <Card v-for="(item, index) in cardContents" :key="index" :judul="item.title" :text="item.text" :image="item.image">
+    <small>created by: @sitamvan</small>
+  </Card>
+</template>
+
+<script>
+// panggil file
+// tanda @ fungsinya untuk = mewakilkan src
+import Card from "@/components/Card.vue";
+export default {
+  // component apa saja yang akan ditampilkan
+  components: {
+    // definisi component agar bisa digunakan di template
+    Card,
+  },
+  // menampung variable
+  data() {
+    return {
+      cardContents: [
+        {
+          title: "Wahyu :",
+          text: `<ul>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, esse!</li>
+                    <li>Lorem ipsum dolor sit amet.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                </ul>`,
+          image: 'satir.jpeg',
+        },
+        {
+          title: "Ucup :",
+          text: `<ul>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, esse!</li>
+                    <li>Lorem ipsum dolor sit amet.</li>
+                    <li>Lorem ipsum dolor sit amet consectetur adipisicing.</li>
+                </ul>`,
+          image: 'lah.jpeg',
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style></style>
---
+++ 12209331 - Okta Haris Sutanto/vue-CARD/src/components/card.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="wrap">
+        <div class="card">
+            <div class="img-wrap">
+            <img :src="'/src/assets/img/'+ image" alt="" srcset="">
+        </div>
+            <div class="text-wrap">
+                <h3>{{judul}}</h3>
+                <slot></slot>
+                <div v-html="text"></div>
+            </div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+    // mengambil data dari luar file
+    props: ['judul', 'text', 'image'],
+}
+</script>
+
+<style scooped>
+.wrap {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+.card {
+    width: 600px;
+    height: auto;
+    border-radius: 5px;
+    border: 2px solid black;
+    display: flex;
+    margin-top: 20px;
+}
+.img-wrap {
+    width: 35%;
+    height: auto;
+    position: relative;
+}
+.text-wrap {
+    width: 65%;
+    height: 100%;
+    padding: 10px;
+}
+.img-wrap img {
+    width: 100%;
+    height: 100%;
+    /* position: absolute; */
+}
+</style>
