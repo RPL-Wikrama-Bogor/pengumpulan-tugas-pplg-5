16c71388e1cf0f9bcac1877e8676a5b8893c4110
Elsa8989 <elsasalsabilla@smkwikrama.sch.id>
2023-11-10 15:07:09 +0700

    kelas industri

---
+++ 12208983 - Elsa Salsa Billa/vue-js/compro-wikrama/src/components/Portofolio/Card.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="card-portofolio">
+        <img :src="portofolio.file" alt="portofolio">
+        <div class="card-portofolio-description">
+            <h4>{{portofolio.title}}</h4>
+            <p><span>Klient: </span>{{ portofolio.name }}</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props:['portofolio']
+}
+</script>
+<style scoped>
+.card-portofolio {
+    text-align: start;
+    min-height: 306px;
+    /* padding: 30px 30px 0 30px; */
+    padding: 0;
+    box-shadow: 0px 10px 30px #879bb5;
+    border-radius: 0px 60px 0px 0px;
+    background: white;
+    transition: all 0.5s ease;
+    margin: 10px 10px;
+}
+.card-portofolio img {
+    max-width: 100%;
+    height: auto;
+    border-radius: 0px 60px 0px 0px;
+}
+.card-portofolio-description {
+    padding: 0 20px 20px 20px;
+}
+.card-portofolio h4{
+    margin-top: 10px;
+    font-weight: 900;
+    font-size: 20px;
+    line-height: 35px;
+    margin-bottom: 0;
+    color: #27374b;
+}
+</style>
---
+++ 12208983 - Elsa Salsa Billa/vue-js/dasar-vue/src/App.vue
@@ -0,0 +1,137 @@
+<template>
+  <!-- template buat naro html nya -->
+  <p>-------------templating-----------</p>
+  {{ nama }}
+  {{ number }}
+  <!-- untuk menampilkan variabel -->
+  <div v-html="kelas"></div>
+  <!-- v- : binding (dinamis) bisa diganti jadi titik dua(:) -->
+  <!-- bukan teks biasa tapi ngambil variabel -->
+  <p>-------------data binding-----------</p>
+  <button :disabled="nonaktif">button</button>
+  <h1 v-bind="property">SMK Wikrama</h1>
+  <input :type="typeInput">
+  <!-- bukan type nya tapi manggil variable -->
+  <p>-------------data binding-----------</p>
+  <!-- ternari -->
+  <!-- if count==0 + 1
+        else + 2 -->
+  {{ count == 0 ? count + 1 : count + 2 }}
+ <!-- kalo kondisinya cuman nampilin teks berarti pake ternari
+      tapi kalo selain teks pakenya binding -->
+
+      <!-- jika lebih dari 1
+      count== 0 ? count + 1? count== 1? count + 2? : ? : -->
+  <p>-------------v-if-----------</p>
+
+  <button v-if="show">submit</button>
+  <button v-show="show">submit</button>
+  <!-- show : hidden -->
+
+  <!-- CONTOH BINDING -->
+  <div v-if="count==1">
+    number 1
+  </div>
+  <div v-else-if="count==2">
+    number 2
+  </div>
+  <div v-else>
+    lebih dari 2
+  </div>
+  <p>-------------computed and methods-----------</p>
+  <input :type="typeInput">
+  <button @click="showPasssword">show password</button>
+
+
+  <!-- <button @click="counterNumber">Method {{ counterButton }}</button>
+  <button @click="countComputed">Computed {{ numberComputed }}</button> -->
+  <p>-------------class and style-----------</p>
+
+  <ul>
+    <!-- kalo lebih dari 1 variabel, binding nya pake kurung kurawal -->
+    <li :class="{active:isActive, fs40px:isActive}">
+      <!-- tipe data nya harus bolean -->
+      test
+    </li>
+  </ul>
+  <button @click="ubahWarna">ubah warna</button>
+  <!-- @click = onclick -->
+  <p>-------------list rendering-----------</p>
+  <ul>
+    <li v-for="(item,index) in daftarKelas">{{ index+1 }}{{ item }}</li>
+    <!-- item = 1, daftarKelas = banyak -->
+    <!-- yg wajib ada itu item -->
+  </ul>
+
+  <p>-------------v-model-----------</p>
+  <input type="text" v-model="Inputkelas">{{ Inputkelas }}
+  <!-- v-model : name -->
+
+</template>
+
+<script>
+//script buat naro js nya
+export default{
+  data(){
+    return{
+      //RETURN UNTUK MENGEMBALIKAN DATA AGAR BISA DIGUNAKAN
+      Inputkelas: ' ',
+      daftarKelas:['RPL 1', 'RPL 2', 'RPL 3'],
+      typeInput: 'password',
+      counterButton: 0,
+      numberComputed:0,
+      show:true,
+      count: 2,
+      nama: 'elsa',
+      number: 3,
+      kelas:'<h1>kelas 11</h1>',
+      nonaktif:true,
+      property:{
+        //tipe data object
+        id:1,
+        class:'color'
+      },
+      typeInput: 'password',
+      isActive:true
+    }
+  },
+  methods:{
+    //function nya bisa dipanggil berkali-kali
+    counterNumber(){
+      this.counterButton += 1;
+    },
+    showPasssword(){
+      if(this.typeInput=='password'){
+        this.typeInput = 'text';
+      } else{
+        this.typeInput = 'password';
+      }
+    },
+    ubahWarna(){
+    if(this.isActive){
+      this.isActive = false;
+    } else {
+      this.isActive = true;
+    }
+  }
+  },
+  computed:{
+    //functionnya cuman bisa dipanggil sekali
+    countComputed(){
+      this.numberComputed += 1;
+    }
+  }
+}
+</script>
+
+<style>
+.color{
+  color:rgb(57, 172, 178);
+}
+.fs40px{
+  font-size: 40px;
+}
+.active{
+  color:brown;
+}
+</style>
