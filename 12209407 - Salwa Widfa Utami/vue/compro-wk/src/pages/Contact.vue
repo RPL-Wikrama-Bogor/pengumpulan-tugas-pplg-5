f5a5a4e115593bab8c7f08c9322a47a434bac191
salwawidfau95 <salwawidfautami@smkwikrama.sch.id>
2023-11-10 15:01:43 +0700

    prindustrisalwa

---
+++ 12209407 - Salwa Widfa Utami/vue/compro-wk/src/pages/Contact.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+    <Blog :data="DataContact"></Blog>
+    </div>
+</template>
+
+<script>
+import Contact from '@/components/Beranda/Contact.vue';
+
+import {Get} from '@/api/index.js';
+export default{
+    components: {
+        Contact,
+    },
+    data(){
+        return{
+            DataContact:[],
+        }
+    },
+    async created() {
+        const responseContact = await Get('http://localhost:9000/api/contact');
+        this.DataContact = responseContact.data.data;        
+    },
+}
+</script>
+
+<style>
+
+</style>
