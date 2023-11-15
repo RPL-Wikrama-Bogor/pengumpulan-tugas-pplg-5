bef1e504870d1c71220ae6a2376a28987d3de0da
Muhammad-Khairul-Kholqi <khairulkhlqi31@gmail.com>
2023-11-15 19:04:35 +0700

    Vue components, routing & fetch API

---
+++ 12209245 - Muhammad Khairul Kholqi/vue-component/src/main.js
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import Btn from "@/components/my-components/Button.vue";
+
+const app = createApp(App);
+app.component("MyBtn", Btn);
+app.mount("#app");
