789de3c34fdeb1a09a2223bd6ca75fdff5c1f682
insyamw <insyamw@gmail.com>
2023-11-16 07:43:40 +0700

    tugas industri

---
+++ 12209053 - Insya Mada Wirda/api-wikrama 2/database/migrations/2023_11_02_022515_portfolios.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Portfolios extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ 12209053 - Insya Mada Wirda/vueJS-master/LPvueJS-master/src/pages/Home.vue
@@ -0,0 +1,49 @@
+<template>
+    
+    <div class="container">
+     <Beranda :data="DataHome"></Beranda>
+     <Service :data="DataLayanan"></Service>
+   <Portfolio :data="DataPortfolio"></Portfolio>
+   <Blog  :data="DataBlog"></Blog>
+    </div>
+   </template>
+   <script>
+
+   import Beranda from '@/components/Beranda/Beranda.vue';
+   import Service from '@/components/Beranda/Service.vue';
+   import Portfolio from '@/components/Beranda/Portofolio.vue';
+   import Blog from '@/components/Beranda/Blog.vue';
+   import { Get } from '../Api';
+   export default {
+     components: {
+       Beranda,
+       Service,
+       Portfolio,
+       Blog
+   },
+   data() {
+    return{
+        DataHome: [],
+        DataLayanan:[],
+        DataPortfolio:[],
+        DataBlog: [],
+    }
+   },
+   async created() {
+    const response = await Get('http://127.0.0.1:9000/api/home');
+    this.DataHome = response.data;
+
+    const responseLayanan = await Get('http://127.0.0.1:9000/api/services');
+    this.DataLayanan = responseLayanan.data.data;
+
+    const responsePorfolio = await Get('http://127.0.0.1:9000/api/portfolio');
+    this.DataPortfolio = responsePorfolio.data.data;
+    console.log(responsePorfolio.data.data);
+
+    const responseBlog = await Get('http://127.0.0.1:9000/api/blog');
+    this.DataBlog = responseBlog.data.data;
+   },
+   }
+   </script>
+   
+   
